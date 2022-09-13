b4cd23ed451e43950216fa2bda537dd2f1abc33a
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-13 18:14:15 +0200

    2nd commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# https://www.youtube.com/watch?v=txKBWtvV99Y
+# 13:45 https://www.youtube.com/watch?v=txKBWtvV99Y
 
 import curses
 from curses import wrapper
@@ -18,14 +18,22 @@ maze = [
 ]
 
 
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.init_pair(1)
+    RED = curses.init_pair(2)
+
+    for i in enumerate(maze):
+
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     blue_and_black = curses.color_pair(1)
 
-    stdscr.clear()
-    stdscr.addstr(5, 5, 'Hello World!')
-    stdscr.refresh()
-    stdscr.getch()
+    # stdscr.clear()
+    # stdscr.addstr(5, 5, 'Hello World!', blue_and_black)
+    # stdscr.refresh()
+    # stdscr.getch()
 
 
 wrapper(main)
